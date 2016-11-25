70296cd5f8641e4c0e4e94a9de771781279b9d0d
Atif <atif.goheer@innovasolutions.com>
2016-11-24 16:40:54 -0800

    WebUI Added to Source Control

---
+++ WebUI/EverywhereWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EverywhereWeb.Startup))]
+namespace EverywhereWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
