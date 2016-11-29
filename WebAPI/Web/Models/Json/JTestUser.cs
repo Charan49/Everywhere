d0a653d83f125311da987e0e615a35ebb510c73b
charanait <laxmana.charan@innovasolutions.com>
2016-11-29 11:53:09 +0200

    Test User Changes

---
+++ WebAPI/Web/Models/Json/JTestUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class JTestUser
+    {
+       
+        public string id;
+        public string access_token;
+        public string login_url;
+        public string email;
+        public string password;
+
+    }
+
+   
+}
